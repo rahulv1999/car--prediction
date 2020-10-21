7bc9cf083c20237e056c331310d432ca620f134a
Rahul Verma <48206612+rahulv1999@users.noreply.github.com>
2020-10-21 11:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,39 +16,6 @@ def predict():
     #to render result in html GUI
     
     if request.method == 'POST':
-        yrs = request.form['yrs']
-        currentprice = request.form['currentprice']
-        km = request.form['km']
-        fueltype = request.form['fueltype']
-        sellertype = request.form['sellertype']
-        transmission = request.form['transmission']
-        owners = request.form['owners']
-        
-        if fueltype == 'petrol':
-            petrol =1
-            deisel =1
-        elif fueltype =='diesel':
-            petrol =0
-            deisel =1
-        else:
-            petrol =0
-            deisel =0
-            
-        if sellertype =='individual':
-            individual = 1
-        else:
-            individual = 0
-            
-        if transmission == 'manual':
-            manual = 1
-        else:
-            manual = 0
-            
-            
-        data = np.array([currentprice, km, owners, yrs, deisel, petrol, individual, manual])
-        data = data.reshape(1,-1)
-        output =  (model.predict(data))
-        k = round(output[0],2)
         f = open('templates/test.txt','w')
         f.write("\n thank god it worked")
         f.close()
