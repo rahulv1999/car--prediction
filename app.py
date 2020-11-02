e35816380ae70f65447bbffbdba14b282032a1f5
Rahul Verma <48206612+rahulv1999@users.noreply.github.com>
2020-11-02 10:18:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('car_prediction.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods = ['POST'])
 def predict():
